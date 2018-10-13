080d51296654136e3448f90d971e6335cfa4b6fc
mahendra <palsinghmahendra.iet@gmail.com>
2018-10-13 16:09:17 +0530

    added missing js

---
+++ public/js/index.js
@@ -0,0 +1,12 @@
+var socket = io();
+socket.on('connect', function () {
+    console.log('connected to the server');
+});
+
+socket.on('disconnect', function () {
+    console.log('disconnected from server');
+});
+
+socket.on('newMessage', function(newMessage) {
+    console.log('New Message', newMessage);
+})
