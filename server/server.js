1098f8139770663428b5d6ebeb3af616d489af21
mahendra <palsinghmahendra.iet@gmail.com>
2018-10-13 16:06:26 +0530

    Added new message and create message event

--- server/server.js
+++ server/server.js
@@ -20,13 +20,13 @@ io.on('connection', (socket) => {
     });
 
     socket.on('createMessage', (newMessage) => {
-        console.log('createEmail', newMessage);
-    });
+        console.log('createMessage', newMessage);
 
-    socket.emit('newMessage', {
-        from: 'mike@example.com',
-        test: 'Hey, what\'s going on',
-        createAt: 123
+        io.emit('newMessage', {
+            from: newMessage.from,
+            text: newMessage.text,
+            createdAt: new Date().getTime()
+        });
     });
 });
 
