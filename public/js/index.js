2658ec5e919d41aa8ea22ca9010237223952e513
mahendra <palsinghmahendra.iet@gmail.com>
2018-10-14 23:41:57 +0530

    added form and showed the incomming messages

--- public/js/index.js
+++ public/js/index.js
@@ -10,4 +10,28 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
   console.log('newMessage', message);
+
+  let li = $('<li></li>');
+  li.text(`${message.from}: ${message.text}`)
+
+  $('#messages').append(li);
+  $('[name=message]').val('');
 });
+
+socket.emit('createMessage', {
+  from: 'frank',
+  text: 'Hi'
+}, function(data){
+  console.log('got it!.', data);
+});
+
+$('#message-from').on('submit', function(e){
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: "User",
+    text: $('[name=message]').val()
+  }, function(data){
+    console.log(data);
+  });
+})
